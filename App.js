4ebcefc88962c78cc81eb08bf6931467ec13e2fb
hrNoh <mtxpvksh@gmail.com>
2022-03-25 15:25:37 +0900

    first test hr

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>first test hrnoh</Text>
+      <Text>first test hrnoh something something</Text>
       <StatusBar style="auto" />
     </View>
   );
