2ba692bd41767c5c57f920524510b98d06c04289
Paksubeen <tnqls520@gmail.com>
2022-03-25 14:55:15 +0900

    Test 1

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>깃허브 커밋 테스트!!!!</Text>
+      <Text>깃허브 테스트(PSB)</Text>
       <StatusBar style="auto" />
     </View>
   );
