adf435d29423b9f1f78f2e02deb3c5d4d8c82872
BaeYuJin <star0417@hansung.ac.kr>
2022-03-25 16:49:42 +0900

    db

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Category from './Category';
 
 export default function App() {
+  Category = Category;
   return (
     <View style={styles.container}>
       <Text>second test hrnoh something something</Text>
