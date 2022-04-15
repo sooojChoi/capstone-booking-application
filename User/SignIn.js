1f3a3455f8f496ace35ecc63956632b6a3f83379
hrNoh <mtxpvksh@gmail.com>
2022-04-15 19:36:58 +0900

    refactoring

--- User/SignIn.js
+++ User/SignIn.js
@@ -1,7 +1,7 @@
 // 회원가입(사용자) -> 혜림
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,TextInput,Button,KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, Text, View,TextInput,Button,KeyboardAvoidingView,TouchableOpacity } from 'react-native';
 
 export default function SignIn() {
   return (
@@ -10,40 +10,51 @@ export default function SignIn() {
     style={styles.container}
   >
     <View style={styles.container}>
-      <View style={{flex:2}}>
-      <Text style={styles.title}>회원가입</Text>
-      </View>
-      <View style={styles.line}>
-      <Text style={styles.text}>이름</Text> 
-      <TextInput style={styles.input}/>
-      </View>
-      <View style={styles.line}>
-      <Text style={styles.text}>ID</Text> 
-      <TextInput style={styles.input}/>
-      <Button title="중복확인"/>
-      </View>
-      <View style={styles.line}>
-      <Text style={styles.text}>비밀번호</Text> 
-      <TextInput style={styles.input}/>
-      </View>
-      <View style={styles.line}>
-      <Text style={styles.text}>재입력</Text> 
-      <TextInput style={styles.input}/>
+          <View style={{flex:2}}>
+              <Text style={styles.title}>회원가입</Text>
+          </View>
+          <View style={styles.line}>
+              <Text style={styles.text}>이름</Text> 
+              <TextInput style={styles.input}/>
+          </View>
+          <View style={styles.line}>
+              <Text style={styles.text}>ID</Text> 
+              <TextInput style={styles.input}/>
+              <TouchableOpacity>
+                  <Text>중복확인</Text>
+              </TouchableOpacity>
+          </View>
+          <View style={styles.line}>
+                <Text style={styles.text}>비밀번호</Text> 
+                <TextInput style={styles.input}/>
+          </View>
+                <View style={styles.line}>
+                <Text style={styles.text}>재입력</Text> 
+                <TextInput style={styles.input}/>
+          
+          </View>
+          <View style={styles.line}>
+                <Text style={styles.text}>전화번호</Text> 
+                <TextInput style={styles.input}  keyboardType="numeric"/>
+                <TouchableOpacity>
+                  <Text>인증</Text>
+              </TouchableOpacity>
+          </View>
+          <View style={styles.line}>
+                <Text style={styles.text}>인증번호</Text> 
+                <TextInput style={styles.input}  keyboardType="numeric"/>
+                <TouchableOpacity>
+                  <Text>확인</Text>
+                </TouchableOpacity>
+         
+          </View>
+          <View style={{flex:2}}>
+            <TouchableOpacity  style={{padding:10,alignItems:'center',borderWidth:1,marginHorizontal:100}}>
+                  <Text style={styles.text}>회원가입</Text>
+          </TouchableOpacity>
+          </View>
+
       
-      </View>
-      <View style={styles.line}>
-      <Text style={styles.text}>전화번호</Text> 
-      <TextInput style={styles.input}  keyboardType="numeric"/>
-      <Button title="인증"/>
-      </View>
-      <View style={styles.line}>
-      <Text style={styles.text}>인증번호</Text> 
-      <TextInput style={styles.input}  keyboardType="numeric"/>
-      <Button title="확인"/>
-      </View>
-      <View style={{flex:2, padding:50,}}>
-      <Button title="회원가입"/>
-      </View>
     </View>
 
     </KeyboardAvoidingView>
@@ -62,13 +73,14 @@ const styles = StyleSheet.create({
   },
   /*모든 텍스트 스타일*/
   text:{
-    paddingTop:10,
     paddingStart:18,
     fontSize:20,
   },
   line:{
+    alignItems:'center',
     flex:1.5,
     flexDirection:"row",
+
   },
   input: {
     height: 40,
