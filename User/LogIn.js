3b2b44e9da44760d4cf1a408a62a86247db981bf
hrNoh <mtxpvksh@gmail.com>
2022-04-15 19:36:44 +0900

    refactoring

--- User/LogIn.js
+++ User/LogIn.js
@@ -1,34 +1,50 @@
 // 로그인(사용자) -> 혜림
 
-import { StyleSheet, Text, View,Image ,TextInput,Button,KeyboardAvoidingView} from 'react-native';
+import { StyleSheet, Text, View,Image ,TextInput,TouchableOpacity,KeyboardAvoidingView,
+  Dimensions } from 'react-native';
+
+/*모바일 윈도우의 크기를 가져와 사진의 크기를 지정한다. styles:FacilityImageStyle*/
+const {height,width}=Dimensions.get("window");
 
 export default function LogIn() {
+
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       style={styles.container}
     >
     <View style={styles.container}>
+{/*로고 이미지 */}
         <Image style={styles.logoImage}
-        source={require('../assets/bbokinglogo2.png')}>
+              source={require('../assets/bbokinglogo2.png')}>
         </Image>
-    <View style={styles.entire}>
-      <View style={styles.loginForm}>
-        <View style={styles.loginInput}>
-          <Text style={styles.text}>ID  </Text>
-          <TextInput style={styles.textinput}></TextInput>
+
+{/*로그인 폼*/}
+        <View style={styles.loginForm}>
+            <View>
+                <View style={styles.loginInput}>
+                    <Text style={styles.text}>ID  </Text>
+                    <TextInput style={styles.textinput}></TextInput>
+                </View>
+                <View style={styles.loginInput}>
+                      <Text style={styles.text}>PW</Text>
+                      <TextInput style={styles.textinput}></TextInput>
+                </View>
+                <View style={styles.signUpBtn}>
+                      <TouchableOpacity>
+                            <Text style={styles.text}>회원가입</Text>
+                      </TouchableOpacity>
+                </View>
+            </View>
+                <View style={{marginTop:10}}>
+                  <TouchableOpacity style={{borderWidth:1,paddingHorizontal:20,paddingVertical:40}}>
+                       <Text style={styles.text}>LogIn</Text>
+                  </TouchableOpacity>
+                </View>  
           </View>
-        <View style={styles.loginInput}>
-          <Text style={styles.text}>PW</Text>
-          <TextInput style={styles.textinput}></TextInput>
-        </View>
-        < Button title="회원가입"/>
-      </View>
-      <View>
-        <Button title="Login"/>
-      </View>
     </View>
-    </View>
+      
     </KeyboardAvoidingView>
   );
 }
@@ -40,10 +56,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   logoImage:{
-    flex: 1,
+    width: width/2,
+    height:height/4,
   },
-  /*입력필드 스타일*/
-  loginForm:{
+  signUpBtn:{
+    alignItems:'flex-end'
   },
   textinput:{
     height: 40,
@@ -57,12 +74,11 @@ const styles = StyleSheet.create({
     alignItems:'center',
   },
   text:{
-    fontSize:30,
-    
+    fontSize:25,
   },
   /*입력필드와 로그인 버튼을 포함하는 전체 뷰의 스타일*/
-  entire:{
-    flex:1,
+  loginForm:{
+    justifyContent:'center',
     flexDirection:'row',
   }
 });
