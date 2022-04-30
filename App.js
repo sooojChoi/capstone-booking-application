635aaa8f1016ff283dc2742bc647be5de00a606d
Paksubeen <tnqls520@gmail.com>
2022-04-30 14:46:01 +0900

    DetailFacilityManagement Modify DB Updated

--- App.js
+++ App.js
@@ -14,20 +14,10 @@ import SignIn from './User/SignIn';
 
 export default function App() { // 확인할 UI의 retrun 문 주석만 제거 후 실행함
   ////////// 관리자(Admin) UI
-  return (<AdminBooking></AdminBooking>) // 대리 예약(유진)
-
-
-  //return (<BookingManagement></BookingManagement>) // 예약 관리(수빈)
-  //return (<DetailBookingManagement></DetailBookingManagement>) //// 상세 예약 관리(수빈)
-
-  //return (<DetailFacilityManagement></DetailFacilityManagement>) // 상세 시설 관리(수빈)
-  //return (<FacilityManagement></FacilityManagement>) // 시설 관리(수빈)
-
- // return (<DetailUserManagement></DetailUserManagement>) // 상세 사용자 관리(수진)
+  //return (<AdminBooking></AdminBooking>) // 대리 예약(유진)
 
   //return (<BookingManagementNavigation></BookingManagementNavigation>) // 예약 관리 -> 상세 예약 관리(수빈)
-  //return (<FacilityManagementNavigation></FacilityManagementNavigation>) // 시설 관리 -> 상세 시설 관리(수빈)
-
+  return (<FacilityManagementNavigation></FacilityManagementNavigation>) // 시설 관리 -> 상세 시설 관리(수빈)
 
   //return (<UserManagementNavigation></UserManagementNavigation>) // 사용자 관리 -> 상세 사용자 관리(수진)
   //return (<UserPermission></UserPermission>) // 사용자 승인(수진)
