1deaeb35af7b149b5ba9584eeb7f3dad3dc2f199
soojin <cmleon88@naver.com>
2022-05-27 21:44:58 +0900

    notification function is added.

--- App.js
+++ App.js
@@ -69,17 +69,17 @@ export default function App() {
   // )
 
   ////////// 역할 선택 & 홈 UI (User 폴더에 위치함)
-  //return (<Home></Home>) // 홈(유진)
+ // return (<Home></Home>) // 홈(유진)
 
   ////////// 관리자(Admin) UI
 
 
-   //return (<AdminWholeStack></AdminWholeStack>)   // 관리자 홈 화면
+   return (<AdminWholeStack></AdminWholeStack>)   // 관리자 홈 화면
 
 
   //return (<AdminBooking></AdminBooking>) // 대리 예약(유진)
 
-  return (<GenerateAllocation></GenerateAllocation>) // 관리자 allocation 생성(혜림)
+  //return (<GenerateAllocation></GenerateAllocation>) // 관리자 allocation 생성(혜림)
 
   // return (<UserManagementNavigation></UserManagementNavigation>) // 사용자 관리(수진)
 
